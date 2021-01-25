7cadf844436d1f6774745683d48c815e1f50e8e7
Asdkasjld69 <cnmbxjj@hotmail.com>
2021-01-25 09:04:45 +0800

    1

--- TextReplacement/src/ok/FilenameReplacement.java
+++ TextReplacement/src/ok/FilenameReplacement.java
@@ -20,93 +20,97 @@ public class FilenameReplacement {
 	private static String path = "";
 	private static int bufferSize = 2048;
 
-	public static String replace(File F,ArrayList<Object[]> srcs,ArrayList<Object> dests,boolean safe, String encode) {
-		
+	public static String replace(File F, ArrayList<Object[]> srcs, ArrayList<Object> dests, boolean safe,
+			String encode) {
+
 		File backup = null;
 		StringBuffer log = new StringBuffer();
 		String filepath = F.getAbsolutePath();
 		String filename = F.getName();
 		Date time = new Date();
-		log.append("<log><message>"+F.getName()+" #STARTED</message><time>"+time.toString()+"</time></log>");
-		if(safe) {
-			backup = new File(path+"/backup-"+serial+filepath.replace(path, "").replace(filename, ""));
-			if(!backup.exists()) {
+		log.append("<log><message>" + F.getName() + " #STARTED</message><time>" + time.toString() + "</time></log>");
+		if (safe) {
+			backup = new File(path + "/backup-" + serial + filepath.replace(path, "").replace(filename, ""));
+			if (!backup.exists()) {
 				backup.mkdirs();
 			}
 		}
 		try {
-			for(int i=0;i<srcs.size();i++) {
-				String match = (String)srcs.get(i)[0];
+			for (int i = 0; i < srcs.size(); i++) {
+				String match = (String) srcs.get(i)[0];
 				String name = filename.substring(0, filename.indexOf("."));
 				String suffix = filename.substring(filename.indexOf("."));
-				System.out.println(name+suffix);
-				if(name.contains(match)) {
-					String value = (String)dests.get(i);
-					if(srcs.get(i).length>1) {
+				System.out.println(name + suffix);
+				if (name.contains(match)) {
+					String value = (String) dests.get(i);
+					if (srcs.get(i).length > 1) {
 						boolean flag = true;
-						int occ = Integer.parseInt((String) srcs.get(i)[1]) ;
+						int occ = Integer.parseInt((String) srcs.get(i)[1]);
 						int index = 0;
-						while(occ>=0) {
+						while (occ >= 0) {
 							index = name.indexOf(match);
-							if(index<0) {
+							if (index < 0) {
 								flag = false;
 								break;
 							}
-							index += occ>0?match.length():0;
+							index += occ > 0 ? match.length() : 0;
 							name = name.substring(index);
 							occ--;
 						}
-						if(flag) {
+						if (flag) {
 							index = filename.lastIndexOf(name);
-							name = name.replaceFirst(match,value);
-							System.out.println(filename.substring(0, index)+"+"+name+"+"+suffix);
+							name = name.replaceFirst(match, value);
+							System.out.println(filename.substring(0, index) + "+" + name + "+" + suffix);
 							filename = filename.substring(0, index).concat(name).concat(suffix);
 						}
-					}
-					else {
-						filename = name.replace(match,value).concat(suffix);
+					} else {
+						filename = name.replace(match, value).concat(suffix);
 					}
 					System.out.println(filename);
 				}
 			}
-			if(safe) {
-				File tmp = new File(backup.getAbsolutePath()+"/"+F.getName());
+			if (safe) {
+				File tmp = new File(backup.getAbsolutePath() + "/" + F.getName());
 				InputStream in = new FileInputStream(F);
 				OutputStream out = new FileOutputStream(tmp);
 				byte[] buffer = new byte[bufferSize];
-        int len;
+				int len;
 
-        while ((len = in.read(buffer)) > 0) {
-            out.write(buffer, 0, len);
-        }
-				
+				while ((len = in.read(buffer)) > 0) {
+					out.write(buffer, 0, len);
+				}
+
 				in.close();
 				out.close();
 			}
 			F.renameTo(new File(filepath.replace(F.getName(), filename)));
 			System.out.println(F.getName());
 			time = new Date();
-			log.append("<log><message>"+F.getName()+" -> "+filename+" #FINISHED</message><time>"+time.toString()+"</time></log>");
+			log.append("<log><message>" + F.getName() + " -> " + filename + " #FINISHED</message><time>"
+					+ time.toString() + "</time></log>");
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			time = new Date();
-			log.append("<log><message>"+F.getName()+" #FAILED</message><time>"+time.toString()+"</time></log>");
+			log.append("<log><message>" + F.getName() + " #FAILED</message><time>" + time.toString() + "</time></log>");
 			e.printStackTrace();
 		}
 		return log.toString();
 	}
+
 	public static long getSerial() {
 		return serial;
 	}
+
 	public static void setSerial(long serial) {
 		FilenameReplacement.serial = serial;
 	}
+
 	public static String getPath() {
 		return path;
 	}
+
 	public static void setPath(String path) {
 		FilenameReplacement.path = path;
 	}
-	
-	
+
 }
